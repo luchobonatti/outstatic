e884b30c59d607f910119027e6ed58acb84c8021
luchobonatti <luciano@bootnode.dev>
2024-06-04 12:54:44 -0300

    export components from root

--- packages/outstatic/src/index.tsx
+++ packages/outstatic/src/index.tsx
@@ -1,5 +1,12 @@
 import './styles.css'
 
+export * from './components'
+// export * from './utils/'
+export * from './graphql/generated'
+
+export * from './context'
+export * from './client/pages'
+
 export * from './app'
 export * from './app/api'
 export * from './types'
